e080f1fd499389b78be92a880e60d80b7bcae02e
HyeonHoKwon <tiz00002fw@myntec.ac.nz>
2016-09-08 17:59:27 +1200

    plz

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,3 +1,25 @@
 // function str(){
 //     document.getElementById("demo").innerHTML = "liam is here";
 // }
+
+
+//페이스북 SDK 초기화   
+window.fbAsyncInit = function() {  
+    FB.init({appId: '1085416961507014', status: true, cookie: true,xfbml: true});      
+};  
+      
+(function(d){  
+   var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];  
+   if (d.getElementById(id)) {return;}  
+   js = d.createElement('script'); js.id = id; js.async = true;  
+   js.src = "//connect.facebook.net/en_US/all.js";  
+   ref.parentNode.insertBefore(js, ref);  
+ }(document));     
+              
+function facebooklogin() {  
+    //페이스북 로그인 버튼을 눌렀을 때의 루틴.  
+        FB.login(function(response) {  
+            var fbname;  
+            var accessToken = response.authResponse.accessToken;  
+        }, {scope: 'publish_actions,user_likes'});  
+}  
