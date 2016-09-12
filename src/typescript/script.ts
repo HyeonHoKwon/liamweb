59224bd6aa524c30431bfc6a8f101dd94703a370
HyeonHoKwon <tiz00002fw@myntec.ac.nz>
2016-09-13 01:47:40 +1200

    plz

--- src/typescript/script.ts
+++ src/typescript/script.ts
@@ -4,3 +4,17 @@ function logOut(){
     localStorage.clear();
     $('#logButton').html('<a href="#"  onclick="facebooklogin()"> Facebook</a>');
 }
+function myfunction(){
+    document.getElementById("h2js").innerHTML = "Liam's history";
+    document.getElementById("pjs").innerHTML = "Hello my name is HyeonHo Kwon and English name is Liam.";
+    document.getElementById("p2js").innerHTML = "I am from South Korea.";
+    document.getElementById("p3js").innerHTML = "I was born in 1994 Feb 23th.";
+    document.getElementById("p4js").innerHTML = "I studyed IT in Pohang Jecheol Technical High School.";
+    document.getElementById("p5js").innerHTML = "Taking this opportunity, I decided to study IT in Yeungnam University and also in Ntec.";
+    document.getElementById("p6js").innerHTML = "After finish first year, I entered the Marine for fulfill my military service of 2 years.";
+    document.getElementById("p7js").innerHTML = "After finish that, i came to NZ by working holiday visa.";
+    document.getElementById("p8js").innerHTML = "that nz life change my life everything.";
+    document.getElementById("p9js").innerHTML = "NZ is amazing, beautiful country. that is reason why i'm living NZ now.";
+    
+
+}
