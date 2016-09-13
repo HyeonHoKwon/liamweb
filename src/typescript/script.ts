f2daa654772f3edbc7c2f976eceeeec4c9e1a20e
HyeonHoKwon <tiz00002fw@myntec.ac.nz>
2016-09-13 13:03:47 +1200

    plz

--- src/typescript/script.ts
+++ src/typescript/script.ts
@@ -15,6 +15,28 @@ function myfunction(){
     document.getElementById("p7js").innerHTML = "After finish that, i came to NZ by working holiday visa.";
     document.getElementById("p8js").innerHTML = "that nz life change my life everything.";
     document.getElementById("p9js").innerHTML = "NZ is amazing, beautiful country. that is reason why i'm living NZ now.";
-    
-
+}
+function myfunction1(){
+    document.getElementById("h2js").innerHTML = "Employment & Education";
+    document.getElementById("pjs").innerHTML = "RECTAL";
+    document.getElementById("p2js").innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Dec 2012 - Sep 2014 Republic Of Korea Marine corps";
+    document.getElementById("p3js").innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Mar 2016 - Oct 2016 Waiter in Obar (CBD NZ)";
+    document.getElementById("p4js").innerHTML = "University";
+    document.getElementById("p5js").innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;2016 - Present Ntec National Diploma in Computing Level 5";
+    document.getElementById("p6js").innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;2012 Yeungnam University in South Korea Computer Engineering";
+    document.getElementById("p7js").innerHTML = "High School";
+    document.getElementById("p8js").innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;2009 - 2012 Pohang Jecheol Technical High School Computer Networking";
+    document.getElementById("p9js").innerHTML = " ";
+}
+function myfunction2(){
+    document.getElementById("h2js").innerHTML = "More about me!";
+    document.getElementById("pjs").innerHTML = "Hobby";
+    document.getElementById("p2js").innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;BJJ (Brazilian Jiu Jitsu)";
+    document.getElementById("p3js").innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Boxing";
+    document.getElementById("p4js").innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;gym";
+    document.getElementById("p5js").innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;soccer";
+    document.getElementById("p6js").innerHTML = "MARRIAGE / RELATIONSHIP STATUS";
+    document.getElementById("p7js").innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;I have girlfreind.";
+    document.getElementById("p8js").innerHTML = " ";
+    document.getElementById("p9js").innerHTML = " ";
 }
